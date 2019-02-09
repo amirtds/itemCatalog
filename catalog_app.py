ccd447c88f6704381e7bf3c6ceb70b815867e51f
Amir Tadrisi <amirtds@gmail.com>
2019-02-09 11:00:42 -0500

    Initial commit

---
+++ catalog_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/catalog/catalogname")
+def show_items_in_category():
+    return render_template("catalogView.html")
+
+
+if __name__ == "__main__":
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
